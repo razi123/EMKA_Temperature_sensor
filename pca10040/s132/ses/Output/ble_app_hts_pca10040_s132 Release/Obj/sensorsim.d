aa1de270345c881427e52d8dc81805992d95220e
Khazi <khazi@fzi.de>
2020-07-06 15:00:40 +0200

    emka_temp1

---
+++ pca10040/s132/ses/Output/ble_app_hts_pca10040_s132 Release/Obj/sensorsim.d
@@ -0,0 +1,5 @@
+Output/ble_app_hts_pca10040_s132\ Release/Obj/sensorsim.o: \
+ C:\Users\khazi\nRF5_SDK_17.0.0_9d13099\components\libraries\sensorsim\sensorsim.c \
+ C:\Users\khazi\nRF5_SDK_17.0.0_9d13099\components\libraries\sensorsim\sensorsim.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52c/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52c/include/stdbool.h
---
+++ pca10040/s132/ses/Output/ble_app_hts_pca10040_s132 Release/Obj/ses_startup_nrf52.d
@@ -0,0 +1,2 @@
+Output/ble_app_hts_pca10040_s132\ Release/Obj/ses_startup_nrf52.o: \
+ C:\Users\khazi\nRF5_SDK_17.0.0_9d13099\modules\nrfx\mdk\ses_startup_nrf52.s
